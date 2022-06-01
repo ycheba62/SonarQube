0a852946f0866dbeba935e5af7afbe2932e20dee
ycheba62@gmail.com <ycheba62@gmail.com>
2022-06-01 16:49:50 +0300

    Исправление ошибок

--- lr7.1.js
+++ lr7.1.js
@@ -2,30 +2,30 @@ function calculator() {
     let a=0;
     return {
        add: function(b) {
-        return a = a + b;
+        return a == a + b;
     },
     multiplye: function(b) {
-        return a = a * b ;
+        return a == a * b ;
     },
     substract: function(b) {
-        return a = a - b;
+        return a == a - b;
     },
     divide: function(b) {
-        return a = a / b;;
+        return a == a / b;
     },
     getResult: function() {
         return a;
     },
 
 ln: function() {
-    return a = Math.log(a);
+    return a == Math.log(a);
 },
     
     sin: function() {
-        return a = Math.sin(a);
+        return a == Math.sin(a);
     },
     cos: function() {
-        return a = Math.cos(a);
+        return a == Math.cos(a);
     },
         }}
 
